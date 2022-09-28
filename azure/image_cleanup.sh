28692bdb1ba11439f19bf8f6579d73ed951e8ada
suryaprasaath <81792262+suryaprasaath@users.noreply.github.com>
2022-09-28 16:29:16 +0530

    Update image_cleanup.sh

--- azure/image_cleanup.sh
+++ azure/image_cleanup.sh
@@ -48,7 +48,7 @@ client_secret="${7}"
 subscription="82f6da0c-972f-4e06-b7f0-36e6a6303f46"
 tenant_id="d138c625-5abd-4cfe-aaf6-4b1e605c1d0d"
 
-az login --service-principal --username="${client_id}" --tenant="${tenant_id}" --password="${client_secret}" --verbose
+az login --username="${client_id}" --tenant="${tenant_id}" --password="${client_secret}" --verbose
 az account set --subscription "${subscription}"
 
 cleanup_image
